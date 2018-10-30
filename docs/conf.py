c9f5ea223fcc4cc50b40ec233bbd7b914c1c1e88
Marko Ristin <marko.ristin@gmail.com>
2018-10-30 12:01:43 +0100

    bumped to 2.2.0 (#43)

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ author = 'Marko Ristin'
 
 
 # The short X.Y version
-version = '2.1'
+version = '2.2'
 # The full version, including alpha/beta/rc tags
-release = '2.1.2'
+release = '2.2.0'
 
 
 # -- General configuration ---------------------------------------------------

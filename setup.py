e7c284dafe21041b5edb7aad603918851e7f5371
radomsak <adamradomski4@gmail.com>
2019-03-01 11:17:06 +0100

    bumped to 2.2.1 (#46)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as fid:
 
 setup(
     name='spurplus',
-    version='2.2.0',
+    version='2.2.1',
     description='Manage remote machines and file operations over SSH.',
     long_description=long_description,
     url='http://github.com/Parquery/spurplus',

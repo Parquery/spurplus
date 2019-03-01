a2dd2ddc21c873ddfdb86a2061845905f4d99f12
Adam Radomski <adam.radomski@parquery.com>
2019-03-01 11:05:01 +0100

    added temppathlib to install requirements

--- setup.py
+++ setup.py
@@ -33,12 +33,10 @@ setup(
     license='License :: OSI Approved :: MIT License',
     keywords='ssh sftp spur paramiko execute remote commands modify files',
     packages=find_packages(exclude=['tests']),
-    install_requires=['spur==0.3.20', 'typing_extensions>=3.6.2.1', 'icontract>=2.0.1,<3'],
+    install_requires=['spur==0.3.20', 'typing_extensions>=3.6.2.1', 'icontract>=2.0.1,<3', 'temppathlib>=1.0.3,<2'],
     extras_require={
-        'dev': [
-            'mypy==0.620', 'pylint==1.8.2', 'yapf==0.20.2', 'tox>=3.0.0', 'temppathlib>=1.0.3,<2', 'coverage>=4.5.1,<5',
-            'pydocstyle>=2.1.1,<3'
-        ]
+        'dev':
+        ['mypy==0.620', 'pylint==1.8.2', 'yapf==0.20.2', 'tox>=3.0.0', 'coverage>=4.5.1,<5', 'pydocstyle>=2.1.1,<3']
     },
     py_modules=['spurplus', 'spurplus.sftp'],
     include_package_data=True,

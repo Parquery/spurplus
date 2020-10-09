da0d26848b4cab1f54b5570133bfc5d671200bbf
Marko Ristin <marko.ristin@gmail.com>
2020-10-09 22:26:38 +0200

    Added support for development in Windows (#64)
    This change fixes precommit.py so that the library can be developed in
    Windows (and not only in Posix, as was previously the case).

--- precommit.py
+++ precommit.py
@@ -31,34 +31,34 @@ def main() -> int:
                 "yapf", "--in-place", "--style=style.yapf", "--recursive", "tests", "spurplus", "setup.py",
                 "precommit.py"
             ],
-            cwd=repo_root.as_posix())
+            cwd=str(repo_root))
     else:
         subprocess.check_call(
             ["yapf", "--diff", "--style=style.yapf", "--recursive", "tests", "spurplus", "setup.py", "precommit.py"],
-            cwd=repo_root.as_posix())
+            cwd=str(repo_root))
 
     print("Mypy'ing...")
-    subprocess.check_call(["mypy", "spurplus", "tests"], cwd=repo_root.as_posix())
+    subprocess.check_call(["mypy", "spurplus", "tests"], cwd=str(repo_root))
 
     print("Pylint'ing...")
-    subprocess.check_call(["pylint", "--rcfile=pylint.rc", "tests", "spurplus"], cwd=repo_root.as_posix())
+    subprocess.check_call(["pylint", "--rcfile=pylint.rc", "tests", "spurplus"], cwd=str(repo_root))
 
     print("Pydocstyle'ing...")
-    subprocess.check_call(["pydocstyle", "spurplus"], cwd=repo_root.as_posix())
+    subprocess.check_call(["pydocstyle", "spurplus"], cwd=str(repo_root))
 
     print("Testing...")
     env = os.environ.copy()
     env['ICONTRACT_SLOW'] = 'true'
 
     subprocess.check_call(
-        ["coverage", "run", "--source", "spurplus", "-m", "unittest", "discover", "tests"], cwd=repo_root.as_posix())
+        ["coverage", "run", "--source", "spurplus", "-m", "unittest", "discover", "tests"], cwd=str(repo_root))
 
     subprocess.check_call(["coverage", "report"])
 
     print("Doctesting...")
-    subprocess.check_call(["python3", "-m", "doctest", (repo_root / "README.rst").as_posix()])
+    subprocess.check_call([sys.executable, "-m", "doctest", str(repo_root / "README.rst")])
     for pth in (repo_root / "spurplus").glob("**/*.py"):
-        subprocess.check_call(["python3", "-m", "doctest", pth.as_posix()])
+        subprocess.check_call([sys.executable, "-m", "doctest", str(pth)])
 
     return 0
 

186a19ee0a159b8b1db2c7523e15bec922300bcb
Marko Ristin <marko.ristin@gmail.com>
2020-10-09 22:34:43 +0200

    Added support for Python 3.7 and 3.8 (#65)
    This patch adds support for Python 3.7 and 3.8 by integrating this
    Python versions into the continious integration.

--- setup.py
+++ setup.py
@@ -24,11 +24,10 @@ setup(
     author='Marko Ristin',
     author_email='marko.ristin@gmail.com',
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Development Status :: 5 - Production/Stable', 'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6', 'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     license='License :: OSI Approved :: MIT License',
     keywords='ssh sftp spur paramiko execute remote commands modify files',
